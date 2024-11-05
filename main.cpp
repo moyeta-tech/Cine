c31eff4a4a2b21a9387dc5553f51639d1e0190f5
fphidi <anitgomezok@gmail.com>
2024-11-05 20:58:58 -0300

    grupo
    Co-Authored-By: moyeta-tech <65986931+moyeta-tech@users.noreply.github.com>
    Co-Authored-By: ma02-coder <140468444+ma02-coder@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 
 #include <QApplication>
 
+#include <iostream>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Cartelera w;
     w.show();
     return a.exec();
+
+    std::cout << "ola";
 }
