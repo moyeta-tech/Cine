76d2607df4fcb74b6f83c15f62e4cff1c675973a
moyeta-tech <octaviomoya05@gmail.com>
2024-11-08 17:57:47 -0300

    Archivo main adentro

--- Proyecto_Cine/main.cpp
+++ Proyecto_Cine/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:Proyecyo_Cine/main.cpp
 #include "pelicula.h"
 
 #include <QApplication>
@@ -13,24 +12,3 @@ int main(int argc, char *argv[])
     return a.exec();
 
 }
-=======
-#include "cartelera.h"
-
-#include <QApplication>
-
-#include "iostream"
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Cartelera w;
-    w.show();
-    return a.exec();
-
-    std::cout << "hola mundo";
-
-    std::cout << "AAAAAAAAAA";
-
-     std::cout << "asdfohla";
-}
->>>>>>> f839cb2e0aef11a33ebe0426f67c5e18f022f8b2:Proyecto_Cine/main.cpp
