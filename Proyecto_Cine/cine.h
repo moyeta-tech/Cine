4a0c060b3d1db2c392baf7a403a4c114208da085
moyeta-tech <octaviomoya05@gmail.com>
2024-11-08 21:58:30 -0300

    Atributos y metodos de la clase como comentarios

--- Proyecto_Cine/cine.h
+++ Proyecto_Cine/cine.h
@@ -14,21 +14,23 @@ class Cine : public QMainWindow
 
 public:
     explicit Cine(QWidget *parent = nullptr);
-    Cine(QString nombre, QString ubicacion);
     ~Cine();
+/*
     QString getNombre;
     void setNombre(QString nombre);
     QString getUbicacion;
     void setUbicacion(QString ubicacion);
     std::vector<int> getSalas();
     void setSalas();
-
+*/
 
 private:
     Ui::Cine *ui;
+/*
     QString Nombre;
     QString Ubicacion;
     std::vector<int> salas;
+*/
 };
 
 #endif // CINE_H
